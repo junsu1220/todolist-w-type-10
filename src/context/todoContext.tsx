dc0be316e3e6805709db9ed0628e50f70e259c10
junsu1220 <crab7157@gmail.com>
2023-03-05 09:26:31 +0900

    husky

---
+++ src/context/todoContext.tsx
@@ -0,0 +1,73 @@
+import {
+  fetchCreateTodo,
+  fetchDeleteTodo,
+  fetchGetTodos,
+  fetchUpdateTodo,
+} from 'apis/todo/todo';
+import { TodoProps } from 'components/todo/TodoItem.type';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { handleError } from 'utils';
+import { TodoContextProps } from './todoContext.type';
+
+const TodoContext = createContext<TodoContextProps>({} as TodoContextProps);
+
+export const TodoProvider = ({ children }: { children: ReactNode }) => {
+  const [todos, setTodos] = useState<TodoProps[]>([]);
+
+  const refreshTodos = async () => {
+    try {
+      const response = await fetchGetTodos();
+      setTodos(response.data);
+    } catch (error) {
+      handleError;
+    }
+  };
+
+  const createTodo: TodoContextProps['createTodo'] = async (props) => {
+    try {
+      await fetchCreateTodo(props);
+      refreshTodos();
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const updateTodo: TodoContextProps['updateTodo'] = async (props) => {
+    try {
+      await fetchUpdateTodo(props);
+      refreshTodos();
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const deleteTodo: TodoContextProps['deleteTodo'] = async (props) => {
+    try {
+      await fetchDeleteTodo(props);
+      refreshTodos();
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  useEffect(() => {
+    refreshTodos();
+  }, []);
+
+  return (
+    <TodoContext.Provider value={{ todos, createTodo, updateTodo, deleteTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export const useTodo = () => {
+  if (!TodoContext) throw new Error('no TodoContext');
+  return useContext(TodoContext);
+};
